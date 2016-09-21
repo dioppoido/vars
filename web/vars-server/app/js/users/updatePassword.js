e5ca921c9e4404ff33e15bb7299634b25b6852e4
Koutaro Doi <kd1166317@st.kobedenshi.ac.jp>
2016-09-21 13:21:37 +0900

    パスワード変更処理を実装

--- web/vars-server/app/js/users/updatePassword.js
+++ web/vars-server/app/js/users/updatePassword.js
@@ -9,16 +9,15 @@ exports.updatePassword = function(userid,newpass){
     const db = mongoose.createConnection('mongodb://mongo/vars');
     const schema = require('../db/schema');
     const users = db.model('users', schema.users);
+    var msg="";   //返り値（メッセージ）
 
     //パスワード変更処理
     users.update({Userid:userid}, {Passwd:newpass},function(err) {
         if (!err) {
             console.log("DB UPDATE完了");
         } else {
-            console.log("getPassword:"+"DB Error.");
-            reject("DB ERROR");
+            console.log("getPassword:" + "DB Error.");
         }
     });
-
 
 };
