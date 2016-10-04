0c9aa3276fa62a6672ce2e34fa8ff3b0c18ab885
土田　昇平 <kd1150777@st.kobedenshi.ac.jp>
2016-10-04 10:47:25 +0900

    ルート設定

---
+++ web/vars-server/routes/eventtop.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var validator = require('validator'); //validatorモジュール宣言
+//特に送り付ける値はなし
+router.get('/', function(req, res) {
+    if(req.session.user && req.session.passflag == false){
+        res.render('eventtop.ejs');
+    } else{
+        res.redirect('/');
+    }
+
+});
+
+
+router.post('/', function(req, res) {
+
+});
+
+module.exports = router;
