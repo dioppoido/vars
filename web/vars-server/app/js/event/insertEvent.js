3bb3a4a39aceb4e576686e10d5cb6d2729e5a3a5
shashanOz <kd1176004@st.kobedenshi.ac.jp>
2016-11-08 13:27:36 +0900

    未完成　土居君に引き継ぎ

---
+++ web/vars-server/app/js/event/insertEvent.js
@@ -0,0 +1,39 @@
+/**
+ * eventInsertクラス
+ * @author 長谷川遼
+ */
+
+ exports.insertEvent = function(EVENTS){
+   const mongoose = require('mongoose');
+   const db = mongoose.createConnection('mongodb://mongo/vars');
+   const schema = require('../db/schema');
+   const Events = db.model('events', schema.events);
+
+   console.log("aaaaaaa");
+
+   //insert処理
+   const insertevents = new Events({
+       'Eventid':EVENTS.eventid,
+       'Eventname':EVENTS.eventname,
+       'Overciew':EVENTS.overview,
+       'Address':EVENTS.address,
+       'displayName':EVENTS.displayName,
+       'Course':EVENTS.course,
+       'Venue':EVENTS.venue,
+       'Date':EVENTS.date,
+       'Createperiod':{
+           'Createstart':EVENTS.createstart,
+           'Createfinish':EVENTS.createfinish
+       },
+       'Voteperiod':{
+           'Votestart':EVENTS.votestart,
+           'Votefinish':EVENTS.votefinish
+       },
+       'Image':EVENTS.image
+
+
+   });
+    console.log("bbbbb");
+    insertevents.save();
+    console.log("cccc");
+ };
--- web/vars-server/routes/eventcreate.js
+++ web/vars-server/routes/eventcreate.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var validator = require('validator'); //validatorモジュール宣言
 var createEvent = require("../app/js/event/createEvent");
 var randomByte = require("../app/js/db/randomByte");
+var insertEvent = require("../app/js/event/insertEvent");
 
 //特に送り付ける値はなし
 router.get('/', function(req, res) {
@@ -16,6 +17,48 @@ router.get('/', function(req, res) {
 
 
 router.post('/', function(req, res) {
+    //DBに入れ込む処理を呼び出す
+    if(req.session.user){
+        var EVENTS = [];
+        var eventid = 4/*req.body.eventid*/;//変更必要あり
+        var eventname = req.body.eventname;
+        var overview = req.body.overview;
+        var address = req.body.address;
+        var displayname = req.body.displayname;
+        var course = req.body.course;
+        var venue = req.body.venue;
+        var date = req.body.date;
+        const WORK_JSON = {
+            'eventid':eventid,
+            'eventname':eventname,
+            'overview':overview,
+            'address':address,
+            'displayName':displayname,
+            'course':course,
+            'venue':venue,
+            'date':date
+        }
+        const CREATE_JSON = {
+            'createstart':req.body.createstart,
+            'createfinish':req.body.createfinish
+        }
+        const VOTE_JSON = {
+            'votestart':req.body.votestart,
+            'votefinish':req.body.votefinish
+        }
+        var image = req.body.image;
+
+        EVENTS.push(WORK_JSON);
+        EVENTS.push(CREATE_JSON);
+        EVENTS.push(VOTE_JSON);
+
+        console.log(EVENTS);
+
+        insertEvent.insertEvent(EVENTS);
+        res.redirect('/');  //行き先不明
+    }else{
+        res.redirect('/');
+    }
 
 });
 
