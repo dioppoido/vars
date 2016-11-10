f2d2d2a072fe5b6cc9c39611073fa08bc4ee2388
Koutaro Doi <kd1166317@st.kobedenshi.ac.jp>
2016-11-10 13:43:44 +0900

    日付処理関係の関数追加

--- web/vars-server/app/js/moment/moment.js
+++ web/vars-server/app/js/moment/moment.js
@@ -3,13 +3,19 @@
  * @author 多田
  */
  var moment = require('moment');
+moment.locale('ja',{weekdays: ["日","月","火","水","木","金","土"]});
+
 //console.log("momentない");
 
 //現在日時を取得
 exports.todate = function(){
- return moment().format('YYYY/MM/D');
+ return moment().format('YYYY/MM/D(dddd)');
 };
 
 exports.parsedate = function(date){
- return moment().format('YYYY/MM/D HH:mm:ss');
+ return moment(date,'YYYY-MM-D HH:mm:ss').format();
+};
+
+exports.parsedate = function(date,format){
+ return moment(date,'YYYY-MM-D HH:mm:ss').format(format);
 };
