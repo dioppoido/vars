b2452049b1193a394070a775c18d1ccba5ee5c79
土田　昇平 <kd1150777@st.kobedenshi.ac.jp>
2016-11-14 13:11:24 +0900

    一時コミット

--- web/vars-server/routes/index.js
+++ web/vars-server/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var getEventList = require("../app/js/event/getEventList.js");//すべてのイベント抽出
+var inSessionEventList = require("../app/js/event/inSessionEventList.js");//すべてのイベント抽出より開催イベントの抽出
+var todate = require("../app/js/moment/moment.js");
+
 /**
  * ログイン情報があるか確認する(session)
  * ログイン情報がなければlogin.jsへ遷移、あればindex.ejsへ遷移
@@ -14,7 +18,17 @@ const loginCheck = function(req, res, next) {
 };
 
 router.get('/', loginCheck, function(req, res) {
-  res.render('index.ejs', {user: req.session.user});
+  getEventList.getEventList().then(function (docs){
+    //一つでもイベントがある場合
+    if(docs!=null){
+      var inSessionEvent=inSessionEventList.inSessionEventList(todate.todate("YYYY/MM/D"),docs,1);
+      res.render('index.ejs',{user: req.session.user,inSessionEventList:inSessionEvent,notFound:docs});
+      //一つもイベントがない場合
+    }else{
+      res.render('index.ejs',{user: req.session.user,notFound:null});
+    }
+  });
+
 });
 
 module.exports = router;
