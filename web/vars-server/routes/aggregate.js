d7b17f2c8c33c5599d7c05986275e65532c72daf
RYOTA TADA <kd1147686@st.kobedenshi.ac.jp>
2016-11-29 14:48:49 +0900

    総合部門（PC）

--- web/vars-server/routes/aggregate.js
+++ web/vars-server/routes/aggregate.js
@@ -42,7 +42,7 @@ router.get('/', function(req, res) {
                 getVote.getVote(docs[0].Eventid).then(function (docs3){
 
                     //集計結果
-                    var full_aggregate =  new Array();
+                    var full_aggregate =  [];
 
                     async.eachSeries(docs2,function(field,next){
                         //部門ごとのJSON
@@ -56,6 +56,7 @@ router.get('/', function(req, res) {
                                 votecnt: docs4.length,
                                 Teamname: field.Teamname
                             };
+                            console.log("voteJson:"+voteJson.Teamname);
                             full_aggregate.push(voteJson);
                             next();
                         }).catch(function(err){
@@ -65,6 +66,7 @@ router.get('/', function(req, res) {
                     }, function complete(err) {
                         if(!err){
                             //分野毎の得票数
+                            console.log("総合"+full_aggregate[0].Teamname);
                             var n=0;
                             var i=0;
                             var vote_aggregate =  new Array();
@@ -114,7 +116,7 @@ router.get('/', function(req, res) {
                                         console.log("votedata仕上げ:" + vote_aggregate[cnt1][cnt2].votecnt+":"+vote_aggregate[cnt1][cnt2].Teamname);
                                     }
                                 }
-                                res.render('aggregate.ejs',{error: "", event: "", team: "", vote: ""});
+                                res.render('voteresult.ejs',{error: "", full_aggregate:full_aggregate});
                             })
 
                             }
