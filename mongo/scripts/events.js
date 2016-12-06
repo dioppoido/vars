658e80b437ed0e92c314f48637fc4505b34dcbd8
Daiki Yamaguchi <kd1172408@st.kobedenshi.ac.jp>
2016-12-06 10:51:22 +0900

    view修正

--- mongo/scripts/events.js
+++ mongo/scripts/events.js
@@ -12,16 +12,16 @@ db.events.insert({
     Fieldid:"it",
     Venue:"南館1階B教室",
     Holdperiod:{
-      Holdstart :"2016/1/1 9:00",
-      Holdfinish : "2018/1/1 15:00"
+      Holdstart :"2016/11/09 9:00",
+      Holdfinish : "2016/11/10 15:00"
    },
     Createperiod:{
-        Createstart:"2016/1/1 9:00",
-        Createfinish:"2018/1/1 15:00"
+        Createstart:"2016/10/4 9:00",
+        Createfinish:"2016/10/11 15:00"
     },
     Voteperiod:{
-        Votestart:"2016/1/1 9:00",
-        Votefinish:"2018/1/1 15:00"
+        Votestart:"2016/10/12 9:00",
+        Votefinish:"2016/10/20 15:00"
     },
     Image:"public/images/noimage.png"
 });
