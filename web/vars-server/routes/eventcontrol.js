182d9303fe0c19592c57ecc9980c59caff7d43e1
土田　昇平 <kd1150777@st.kobedenshi.ac.jp>
2016-12-08 10:50:50 +0900

    イベント管理route再設定

--- web/vars-server/routes/eventcontrol.js
+++ web/vars-server/routes/eventcontrol.js
@@ -1,47 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var getEvent = require("../app/js/event/getEvent");
 
 router.get('/', function(req, res) {
     if(req.session.user){
-        res.render('eventcontrol.ejs');
+        if(req.query.eventid){
+            var eventid=req.query.eventid;
+            getEvent.getEvent(eventid).then(function (docs) {
+                if(req.query.category){
+                    var category = req.query.category;
+                    switch(category){
+                        case "event":
+                            res.render('eventsetting.ejs');
+                            break;
+                        case "vote" :
+                            res.render('votesetting.ejs');
+                            break;
+                        case "field" :
+                            res.render('fieldsetting.ejs');
+                            break;
+                        case "announce" :
+                            res.render('announcesetting.ejs');
+                            break;
+                        default:
+                            res.render('eventcontrol.ejs', {eventdata: docs});
+                            break;
+                    }
+                }else{
+                    res.render('eventcontrol.ejs', {eventdata: docs});
+                }
+            }).catch(function(){
+                res.render('confirmation.ejs',{msg:'イベントIDが存在しません',url:'/eventlist'});
+            });
+        }else{
+            res.redirect('/eventlist');
+        }
     } else{
         res.redirect('/');
     }
 
 });
-
-router.get('/eventsetting', function(req, res) {
-    if(req.session.user){
-        res.render('eventsetting.ejs');
-    } else{
-        res.redirect('/');
-    }
-});
-
-router.get('/votesetting', function(req, res) {
-    if(req.session.user){
-        res.render('votesetting.ejs');
-    } else{
-        res.redirect('/');
-    }
-});
-
-router.get('/fieldsetting', function(req, res) {
-    if(req.session.user){
-        res.render('fieldsetting.ejs');
-    } else{
-        res.redirect('/');
-    }
-});
-
-router.get('/announcesetting', function(req, res) {
-    if(req.session.user){
-        res.render('announcesetting.ejs');
-    } else{
-        res.redirect('/');
-    }
-});
-
-
 
 module.exports = router;
