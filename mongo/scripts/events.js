98d22aa27906d60dfd83740e682d92165d595a47
shashanOz <kd1176004@st.kobedenshi.ac.jp>
2016-12-15 09:57:04 +0900

    テストデータを追加したscriptファイルを追加

--- mongo/scripts/events.js
+++ mongo/scripts/events.js
@@ -3,345 +3,330 @@ db = db.getSiblingDB("vars");
 db.events.drop();
 db.createCollection("events");
 db.events.insert({
-    Eventid: "1",
-    Eventname: "イベントA",
-    Overview: "イベントAの説明です",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "南館1階B教室",
-    Holdperiod: {
-        Holdstart: "2016/11/09 9:00",
-        Holdfinish: "2018/11/10 15:00"
+    Eventid:"1",
+    Eventname:"イベントA",
+    Overview:"イベントAの説明です",
+    Password:"",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"南館1階B教室",
+    Holdperiod:{
+      Holdstart :"2016/11/09 9:00",
+      Holdfinish : "2018/11/10 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/10/4 9:00",
+        Createfinish:"2018/10/11 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2018/10/11 15:00"
+    Voteperiod:{
+        Votestart:"2016/10/12 9:00",
+        Votefinish:"2018/10/20 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/10/12 9:00",
-        Votefinish: "2018/10/20 15:00"
-    },
-    Image: "public/images/noimage.png",
-    Order: ['2', '3', '6', '1', '5']
+    Image:"public/images/noimage.png"
 });
 
 db.events.insert({
-    Eventid: "0",
-    Eventname: "イベント9",
-    Overview: "イベント9の説明です",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "南館1階F教室",
-    Holdperiod: {
-        Holdstart: "2016/12/1 9:00",
-        Holdfinish: "2016/12/1 15:00"
+    Eventid:"0",
+    Eventname:"イベント9",
+    Overview:"イベント9の説明です",
+    Password:"",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"南館1階F教室",
+    Holdperiod:{
+      Holdstart :"2016/12/1 9:00",
+      Holdfinish : "2016/12/1 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/12/1 9:00",
+        Createfinish:"2016/12/1 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/12/1 9:00",
-        Createfinish: "2016/12/1 15:00"
+    Voteperiod:{
+        Votestart:"2016/12/1 9:00",
+        Votefinish:"2016/12/1 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/12/1 9:00",
-        Votefinish: "2016/12/1 15:00"
-    },
-    Image: "public/images/noimage.png",
-    Order: []
+    Image:"public/images/noimage.png"
 });
 
 db.events.insert({
-    Eventid: "101",
-    Eventname: "イベントZ",
-    Overview: "イベントZno説明です",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "南館1階B教室",
-    Holdperiod: {
-        Holdstart: "2016/11/25 10:00",
-        Holdfinish: "2016/11/25 15:00"
+    Eventid:"101",
+    Eventname:"イベントZ",
+    Overview:"イベントZno説明です",
+    Password:"",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"南館1階B教室",
+    Holdperiod:{
+      Holdstart :"2016/11/25 10:00",
+      Holdfinish : "2016/11/25 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/10/4 9:00",
+        Createfinish:"2016/10/11 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2016/10/11 15:00"
+    Voteperiod:{
+        Votestart:"2016/10/12 9:00",
+        Votefinish:"2016/10/20 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/10/12 9:00",
-        Votefinish: "2016/10/20 15:00"
-    },
-    Image: "public/images/noimage.png",
-    Order: []
+    Image:"public/images/noimage.png"
 });
 db.events.insert({
-    Eventid: "102",
-    Eventname: "イベントP",
-    Overview: "イベントZno説明です",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "南館1階B教室",
-    Holdperiod: {
-        Holdstart: "2016/11/25 12:00",
-        Holdfinish: "2016/11/25 15:00"
+    Eventid:"102",
+    Eventname:"イベントP",
+    Overview:"イベントZno説明です",
+    Password:"",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"南館1階B教室",
+    Holdperiod:{
+      Holdstart :"2016/11/25 12:00",
+      Holdfinish : "2016/11/25 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/10/4 9:00",
+        Createfinish:"2016/10/11 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2016/10/11 15:00"
+    Voteperiod:{
+        Votestart:"2016/10/12 9:00",
+        Votefinish:"2016/10/20 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/10/12 9:00",
-        Votefinish: "2016/10/20 15:00"
-    },
-    Image: "public/images/noimage.png",
-    Order: []
+    Image:"public/images/noimage.png"
 });
 db.events.insert({
-    Eventid: "2",
-    Eventname: "イベントB",
-    Overview: "イベントBの説明です",
-    Password: "password",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "hard",
-    Venue: "ホワイエ",
-    Holdperiod: {
-        Holdstart: "2016/10/21 9:00",
-        Holdfinish: "2016/10/23 15:00"
+    Eventid:"2",
+    Eventname:"イベントB",
+    Overview:"イベントBの説明です",
+    Password:"password",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"hard",
+    Venue:"ホワイエ",
+    Holdperiod:{
+      Holdstart :"2016/10/21 9:00",
+      Holdfinish : "2016/10/23 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/10/12 9:00",
+        Createfinish:"2016/10/20 12:00"
     },
-    Createperiod: {
-        Createstart: "2016/10/12 9:00",
-        Createfinish: "2016/10/20 12:00"
+    Voteperiod:{
+        Votestart:"2016/10/21 9:00",
+        Votefinish:"2016/10/31 12:00"
     },
-    Voteperiod: {
-        Votestart: "2016/10/21 9:00",
-        Votefinish: "2016/10/31 12:00"
-    },
-    Image: "public/images/goripic/drinkgori.png",
-    Order: ['5']
-});
+    Image:"public/images/goripic/drinkgori.png"
+  });
 db.events.insert({
-    Eventid: "3",
-    Eventname: "イベントC",
-    Overview: "イベントCの説明です",
-    Password: "password",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "game",
-    Venue: "ソニックホール",
-    Holdperiod: {
-        Holdstart: "2016/11/01 9:00",
-        Holdfinish: "2016/11/03 15:00"
+    Eventid:"3",
+    Eventname:"イベントC",
+    Overview:"イベントCの説明です",
+    Password:"password",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"game",
+    Venue:"ソニックホール",
+    Holdperiod:{
+      Holdstart :"2016/11/01 9:00",
+      Holdfinish : "2016/11/03 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/10/21 12:00",
+        Createfinish:"2016/10/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/10/21 12:00",
-        Createfinish: "2016/10/31 15:00"
+    Voteperiod:{
+        Votestart:"2016/11/1 12:00",
+        Votefinish:"2016/11/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/11/1 12:00",
-        Votefinish: "2016/11/10 15:00"
-    },
-    Image: "public/images/goripic/fightgori.png",
-    Order: []
-});
+    Image:"public/images/goripic/fightgori.png"
+  });
 
 db.events.insert({
-    Eventid: "4",
-    Eventname: "Soft4合評会",
-    Overview: "SOft4合評会です。",
-    Address: "test@example.com",
-    Password: "password",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "ちか",
-    Holdperiod: {
-        Holdstart: "2016/12/1 9:00",
-        Holdfinish: "2016/12/3 15:00"
+    Eventid:"4",
+    Eventname:"Soft4合評会",
+    Overview:"SOft4合評会です。",
+    Address:"test@example.com",
+    Password:"password",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"ちか",
+    Holdperiod:{
+      Holdstart :"2016/12/1 9:00",
+      Holdfinish : "2016/12/3 15:00"
+   },
+    Createperiod:{
+        Createstart:"2016/12/21 12:00",
+        Createfinish:"2016/12/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2016/12/21 12:00",
-        Createfinish: "2016/12/31 15:00"
+    Voteperiod:{
+        Votestart:"2016/12/1 12:00",
+        Votefinish:"2016/12/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2016/12/1 12:00",
-        Votefinish: "2016/12/10 15:00"
-    },
-    Image: "public/images/goripic/gori.png",
-    Order: []
-});
+    Image:"public/images/goripic/gori.png"
+  });
 db.events.insert({
-    Eventid: "5",
-    Eventname: "2017デジタルワークス",
-    Overview: "2017年度のデジタルワークスです。",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "ちか",
-    Holdperiod: {
-        Holdstart: "2017/12/11 9:00",
-        Holdfinish: "2017/12/13 15:00"
+    Eventid:"5",
+    Eventname:"2017デジタルワークス",
+    Overview:"2017年度のデジタルワークスです。",
+    Password:"",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"ちか",
+    Holdperiod:{
+      Holdstart :"2017/12/11 9:00",
+      Holdfinish : "2017/12/13 15:00"
+   },
+    Createperiod:{
+        Createstart:"2017/12/21 12:00",
+        Createfinish:"2017/12/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2017/12/21 12:00",
-        Createfinish: "2017/12/31 15:00"
+    Voteperiod:{
+        Votestart:"2017/12/1 12:00",
+        Votefinish:"2017/12/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2017/12/1 12:00",
-        Votefinish: "2017/12/10 15:00"
-    },
-    Image: "public/images/goripic/gori2.png",
-    Order: []
-});
+    Image:"public/images/goripic/gori2.png"
+  });
 db.events.insert({
-    Eventid: "6",
-    Eventname: "2018デジタルワークス",
-    Overview: "2018年度のデジタルワークスです。",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "ちか",
-    Holdperiod: {
-        Holdstart: "2018/12/15 9:00",
-        Holdfinish: "2018/12/17 15:00"
+    Eventid:"6",
+    Eventname:"2018デジタルワークス",
+    Overview:"2018年度のデジタルワークスです。",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"ちか",
+    Holdperiod:{
+      Holdstart :"2018/12/15 9:00",
+      Holdfinish : "2018/12/17 15:00"
+   },
+    Createperiod:{
+        Createstart:"2018/12/21 12:00",
+        Createfinish:"2018/12/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2018/12/21 12:00",
-        Createfinish: "2018/12/31 15:00"
+    Voteperiod:{
+        Votestart:"2018/12/1 12:00",
+        Votefinish:"2018/12/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2018/12/1 12:00",
-        Votefinish: "2018/12/10 15:00"
-    },
-    Image: "public/images/goripic/heartgori.png",
-    Order: []
-});
+    Image:"public/images/goripic/heartgori.png"
+  });
 db.events.insert({
-    Eventid: "7",
-    Eventname: "2019デジタルワークス",
-    Overview: "2019年度のデジタルワークスです。",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "ちか",
-    Holdperiod: {
-        Holdstart: "2019/12/16 9:00",
-        Holdfinish: "2019/12/18 15:00"
+    Eventid:"7",
+    Eventname:"2019デジタルワークス",
+    Overview:"2019年度のデジタルワークスです。",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"ちか",
+    Holdperiod:{
+      Holdstart :"2019/12/16 9:00",
+      Holdfinish : "2019/12/18 15:00"
+   },
+    Createperiod:{
+        Createstart:"2019/12/21 12:00",
+        Createfinish:"2019/12/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2019/12/21 12:00",
-        Createfinish: "2019/12/31 15:00"
+    Voteperiod:{
+        Votestart:"2019/12/1 12:00",
+        Votefinish:"2019/12/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2019/12/1 12:00",
-        Votefinish: "2019/12/10 15:00"
-    },
-    Image: "public/images/goripic/nakugori.png",
-    Order: []
-});
+    Image:"public/images/goripic/nakugori.png"});
 
 db.events.insert({
-    Eventid: "8",
-    Eventname: "2020デジタルワークス",
-    Overview: "2020年度のデジタルワークスです。",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "it",
-    Venue: "ちか",
-    Holdperiod: {
-        Holdstart: "2020/12/17 9:00",
-        Holdfinish: "2020/12/19 15:00"
+    Eventid:"8",
+    Eventname:"2020デジタルワークス",
+    Overview:"2020年度のデジタルワークスです。",
+    Address:"test@example.com",
+    displayName:"test",
+    Fieldid:"it",
+    Venue:"ちか",
+    Holdperiod:{
+      Holdstart :"2020/12/17 9:00",
+      Holdfinish : "2020/12/19 15:00"
+   },
+    Createperiod:{
+        Createstart:"2020/12/21 12:00",
+        Createfinish:"2020/12/31 15:00"
     },
-    Createperiod: {
-        Createstart: "2020/12/21 12:00",
-        Createfinish: "2020/12/31 15:00"
+    Voteperiod:{
+        Votestart:"2020/12/1 12:00",
+        Votefinish:"2020/12/10 15:00"
     },
-    Voteperiod: {
-        Votestart: "2020/12/1 12:00",
-        Votefinish: "2020/12/10 15:00"
-    },
-    Image: "public/images/goripic/nicegori.png",
-    Order: []
-});
+    Image:"public/images/goripic/nicegori.png"
+  });
 
 db.events.insert({
-    Password: "",
-    Eventid: "10",
-    Eventname: "文化祭",
-    Overview: "文化祭のガラオケ募集",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "all",
-    Venue: "本館地下１F",
-    Holdperiod: {
-        Holdstart: "2016/10/30 9:00",
-        Holdfinish: "2016/11/1 15:00"
-    },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2016/10/11 15:00"
-    },
-    Voteperiod: {
-        Votestart: "2016/10/30 9:00",
-        Votefinish: "2016/10/31 15:00"
-    },
-    Image: "public/images/goripic/yeahgori.png",
-    Order: []
-});
+  Password:"",
+Eventid:"10",
+Eventname:"文化祭",
+Overview:"文化祭のガラオケ募集",
+Address:"test@example.com",
+displayName:"test",
+Fieldid:"all",
+Venue:"本館地下１F",
+Holdperiod:{
+  Holdstart :"2016/10/30 9:00",
+  Holdfinish : "2016/11/1 15:00"
+},
+Createperiod:{
+    Createstart:"2016/10/4 9:00",
+    Createfinish:"2016/10/11 15:00"
+},
+Voteperiod:{
+    Votestart:"2016/10/30 9:00",
+    Votefinish:"2016/10/31 15:00"
+},
+    Image:"public/images/goripic/yeahgori.png"
+  });
 
 db.events.insert({
-    Eventid: "16",
-    Eventname: "クリスマス",
-    Overview: "クリぼっち募集",
-    Password: "",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "all",
-    Venue: "本館地下10F",
-    Holdperiod: {
-        Holdstart: "2016/12/24 9:00",
-        Holdfinish: "2016/12/25 15:00"
-    },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2016/10/11 15:00"
-    },
-    Voteperiod: {
-        Votestart: "2016/12/25 9:00",
-        Votefinish: "2016/12/25 15:00"
-    },
-    Image: "public/images/goripic/tameikigori.png",
-    Order: []
-});
+Eventid:"16",
+Eventname:"クリスマス",
+Overview:"クリぼっち募集",
+Password:"",
+Address:"test@example.com",
+displayName:"test",
+Fieldid:"all",
+Venue:"本館地下10F",
+Holdperiod:{
+  Holdstart :"2016/12/24 9:00",
+  Holdfinish : "2016/12/25 15:00"
+},
+Createperiod:{
+    Createstart:"2016/10/4 9:00",
+    Createfinish:"2016/10/11 15:00"
+},
+Voteperiod:{
+    Votestart:"2016/12/25 9:00",
+    Votefinish:"2016/12/25 15:00"
+},
+    Image:"public/images/goripic/tameikigori.png"
+  });
 
 
 db.events.insert({
-    Eventid: "12",
-    Eventname: "2016ゲーム分野１年合評会",
-    Overview: "2016ゲーム分野１年合評会をしますの",
-    Password: "1111",
-    Address: "test@example.com",
-    displayName: "test",
-    Fieldid: "game",
-    Venue: "本館地下0F",
-    Holdperiod: {
-        Holdstart: "2016/12/25 9:00",
-        Holdfinish: "2016/12/26 15:00"
-    },
-    Createperiod: {
-        Createstart: "2016/10/4 9:00",
-        Createfinish: "2016/10/11 15:00"
-    },
-    Voteperiod: {
-        Votestart: "2016/12/25 9:00",
-        Votefinish: "2016/12/25 15:00"
-    },
-    Image: "public/images/noimage.png",
-    Order: []
+Eventid:"12",
+Eventname:"2016ゲーム分野１年合評会",
+Overview:"2016ゲーム分野１年合評会をしますの",
+Password:"1111",
+Address:"test@example.com",
+displayName:"test",
+Fieldid:"game",
+Venue:"本館地下0F",
+Holdperiod:{
+  Holdstart :"2016/12/25 9:00",
+  Holdfinish : "2016/12/26 15:00"
+},
+Createperiod:{
+    Createstart:"2016/10/4 9:00",
+    Createfinish:"2016/10/11 15:00"
+},
+Voteperiod:{
+    Votestart:"2016/12/25 9:00",
+    Votefinish:"2016/12/25 15:00"
+},
+Image:"public/images/noimage.png"
 });
 
 db.events.insert({
@@ -381,12 +366,36 @@ Voteperiod:{
         Holdfinish : "2017/12/14 12:00"
      },
       Createperiod:{
-          Createstart:"2017/9/10 12:00",
-          Createfinish:"2017/12/10 15:00"
+          Createstart:"2016/9/10 12:00",
+          Createfinish:"2016/12/10 15:00"
       },
       Voteperiod:{
-          Votestart:"2017/12/14 9:00",
-          Votefinish:"2017/12/14 15:00"
+          Votestart:"2016/12/10 9:00",
+          Votefinish:"2016/12/14 15:00"
       },
       Image:"public/images/goripic/dancegori.png"
     });
+
+    db.events.insert({
+        Eventid:"15",
+        Eventname:"平成27年度デジタルワークス",
+        Overview:"中間発表用テストデータ 去年のデジタルワークス",
+        Password:"",
+        Address:"test@example.com",
+        displayName:"test",
+        Fieldid:"it",
+        Venue:"神戸産業振興センター",
+        Holdperiod:{
+          Holdstart :"2016/2/14 9:00",
+          Holdfinish : "2017/2/14 17:00"
+       },
+        Createperiod:{
+            Createstart:"2016/9/10 12:00",
+            Createfinish:"2016/12/10 15:00"
+        },
+        Voteperiod:{
+            Votestart:"2016/12/10 9:00",
+            Votefinish:"2017/2/15 15:00"
+        },
+        Image:"public/images/goripic/dancegori.png"
+      });
