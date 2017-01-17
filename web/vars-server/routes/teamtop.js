60c1783d34b328a0c3c9a68be3794943556e5fb2
Koutaro Doi <kd1166317@st.kobedenshi.ac.jp>
2017-01-17 10:41:42 +0900

    チームトップページのGET時に参加部門を取り出す処理を追加 ユーザーテーブルを変更。

--- web/vars-server/routes/teamtop.js
+++ web/vars-server/routes/teamtop.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var getTeam = require("../app/js/team/getTeam");
+var getVote = require('../app/js/votes/getVote');
 var nl2br = require("../app/js/nl2br/nl2br.js");
+var async = require('async');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -16,7 +18,20 @@ router.get('/', function(req, res, next) {
                     res.render('confirmation.ejs',{msg:'チームIDが存在しません',url:'/eventlist'});
                 }else {
                     var overview=nl2br.textarea(teamdata[0].Overview);
-                    res.render('teamtop.ejs',{teamdata: teamdata, overview:overview});
+                    var votename=[];
+                    async.eachSeries(teamdata[0].Department,function (department,next) {
+                        getVote.getVotejson({Voteid:department}).then(function (votedata) {
+                            if(votedata.length>0) {
+                                votename.push(votedata[0].Votename);
+                            }
+                            next();
+                        }).catch(function (msg) {
+                            res.render('errorconfirmation.ejs', {msg: msg, url: '/eventtop?eventid=' + eventid});
+                        })
+                    },function (err) {
+                        console.log(votename);
+                        res.render('teamtop.ejs',{teamdata: teamdata, overview:overview, votename:votename});
+                    })
                 }
             });
         }else{
