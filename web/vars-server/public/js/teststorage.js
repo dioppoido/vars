14c06f56adce3c04433a8ba44104d23ad1d798a9
Daiki Yamaguchi <kd1172408@st.kobedenshi.ac.jp>
2017-01-23 13:54:27 +0900

    部門表示

---
+++ web/vars-server/public/js/teststorage.js
@@ -0,0 +1,12 @@
+function teststorage(test){
+var st = localStorage;
+// var user=test;
+// st.setItem('userId', user);
+// var result = st.getItem('userId');
+// console.log(result);    // 10
+
+// st.clear();
+// result = st.getItem('userId');
+// console.log(result);    // null
+// s.clear();
+}
